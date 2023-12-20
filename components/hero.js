fe4a1e2c4dc1497fc9870e021ebe7116296a8f9d
itc-s23009 <s23009@std.it-college.ac.jp>
2023-12-20 05:17:33 +0000

    P102~P118
    end

--- components/hero.js
+++ components/hero.js
@@ -1,15 +1,13 @@
 import styles from 'styles/hero.module.css'
 const Hero = ({ title, subtitle, imageOn = false }) => {
   return (
-    <main>
-      <div>
-        <div className={styles.text}>
-          <h1 className={styles.title}>{title}</h1>
-          <p className={styles.subtitle}>{subtitle}</p>
-        </div>
-        {imageOn && <figure>[画像] </figure>}
+    <div className={styles.flexContainer}>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
       </div>
-    </main>
+      {imageOn && <figure>[画像] </figure>}
+    </div>
   )
 }
 export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 // Import with a different name
 import HeroComponent from 'components/hero'
+import Container from 'components/container'
 
 // Use a different name for the constant
 const CustomHero = () => {
   return (
-    <HeroComponent title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+    <Container>
+      <HeroComponent
+        title='CUBE'
+        subtitle='アウトプットしていくサイト'
+        imageOn
+      />
+    </Container>
   )
 }
 
