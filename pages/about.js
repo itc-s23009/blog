60c326e4161c279efc7706342c231a943db81d52
itc-s23009 <s23009@std.it-college.ac.jp>
2023-12-21 07:35:56 +0000

    5.5 finish

--- pages/about.js
+++ pages/about.js
@@ -7,11 +7,23 @@ import {
   TwoColumnMain,
   TwoColumnSidebar
 } from 'components/two-column'
+import Image from 'next/image'
+import eyecatch from 'next-react-website/blog-app-router/images/about.jpg'
 
 const About = () => {
   return (
     <Container>
       <Hero title='About' subtitle='About development activities' />
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=''
+          layout='responsive'
+          sizes='(min-width: 1152px) 1152px, 100vw'
+          priority
+          placehoder='blur'
+        />
+      </figure>
       <TwoColumn>
         <TwoColumnMain>
           <PostBody>
