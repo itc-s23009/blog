97923b2fac56f2b6098537607b18a8450a106aa9
itc-s23009 <s23009@std.it-college.ac.jp>
2024-01-08 06:17:10 +0000

    6.2_finish favicon(about)

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Hero from 'components/hero'
 import Container from 'components/container'
 import PostBody from 'components/post-body'
@@ -13,6 +14,9 @@ import eyecatch from 'next-react-website/blog-app-router/images/about.jpg'
 const About = () => {
   return (
     <Container>
+      <Head>
+        <title>アバウト</title>
+      </Head>
       <Hero title='About' subtitle='About development activities' />
       <figure>
         <Image
