0aed4194db441279588ab30b7d8cd569f9f46296
itc-s23009 <s23009@std.it-college.ac.jp>
2024-01-10 07:06:12 +0000

    8.1fin 記事のヘッダーの作成

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,14 +1,16 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
+import PostHeader from 'components/post-header'
 
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
   return (
     <Container>
-      <h1>{title}</h1>
+      <article>
+        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+      </article>
     </Container>
   )
 }
-
 export default Schedule
 
 export async function getStaticProps () {
@@ -19,7 +21,7 @@ export async function getStaticProps () {
     props: {
       title: post.title,
       publish: post.publishDate,
-      content: post.content, // 修正: constent から content に修正
+      content: post.content,
       eyecatch: post.eyecatch,
       categories: post.categories
     }
