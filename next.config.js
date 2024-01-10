8840b4f21196262e2f9cc6c1ec994663d14055ec
itc-s23009 <s23009@std.it-college.ac.jp>
2024-01-10 07:17:44 +0000

    8.3 fin アイキャッチ画像をnext/imageで最適化

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domeins: ['images.microcms-assets.io']
+  }
 }
 
 module.exports = nextConfig
