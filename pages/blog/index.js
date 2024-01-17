924819b0a6887bc1a3793a8a74d03ad09830f198
itc-s23009 <s23009@std.it-college.ac.jp>
2024-01-17 03:40:22 +0000

    9.5 fin blogに記事一覧を作成する

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,26 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
-const blog = () => {
+const blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero title='Blog' subtitle='Recent Posts' />
+      <Posts posts={posts} />
     </Container>
   )
 }
 export default blog
+
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+  return {
+    props: {
+      posts
+    }
+  }
+}
+export { getStaticProps }
