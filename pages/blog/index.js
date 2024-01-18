44cd59786725f13fa2c481897b9dd8f42904137e
itc-s23009 <s23009@std.it-college.ac.jp>
2024-01-18 07:59:21 +0000

    object.prototype.hasOwnProperty.call を追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -21,7 +21,7 @@ const getStaticProps = async () => {
   const posts = await getAllPosts()
 
   for (const post of posts) {
-    if (!post.hasOwnProperty.call('eyecatch')) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
       post.eyecatch = eyecatchLocal
     }
     const { base64 } = await getPlaiceholder(post.eyecatch.url)
